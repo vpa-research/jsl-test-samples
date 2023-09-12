3663c8642999639e7497d788fa4bc23ed08eba7b
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2023-09-12 20:51:09 +0300

    Added skeleton generator output sample

---
+++ src/main/java/tests/java/util/OptionalInt_Tests.java
@@ -0,0 +1,116 @@
+package tests.java.util;
+
+import java.util.OptionalInt;
+
+public final class OptionalInt_Tests {
+
+    // internal variables
+
+    // constructors
+
+    // OptionalIntAutomaton::OptionalInt
+    public static int test_OptionalInt_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::OptionalInt (int)
+    public static int test_OptionalInt_1(final int execution) {
+        return -1;
+    }
+
+
+    // static methods
+
+    // OptionalIntAutomaton::empty
+    public static int test_empty_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::of (int)
+    public static int test_of_0(final int execution) {
+        return -1;
+    }
+
+
+    // methods
+
+    // OptionalIntAutomaton::equals (Object)
+    public static int test_equals_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::getAsInt
+    public static int test_getAsInt_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::hashCode
+    public static int test_hashCode_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::ifPresent (IntConsumer)
+    public static int test_ifPresent_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::ifPresentOrElse (IntConsumer, Runnable)
+    public static int test_ifPresentOrElse_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::isEmpty
+    public static int test_isEmpty_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::isPresent
+    public static int test_isPresent_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::orElse (int)
+    public static int test_orElse_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::orElseGet (IntSupplier)
+    public static int test_orElseGet_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::orElseThrow
+    public static int test_orElseThrow_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::orElseThrow (Supplier)
+    public static int test_orElseThrow_1(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::stream
+    public static int test_stream_0(final int execution) {
+        return -1;
+    }
+
+
+    // OptionalIntAutomaton::toString
+    public static int test_toString_0(final int execution) {
+        return -1;
+    }
+
+}
