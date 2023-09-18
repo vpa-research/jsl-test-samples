311bd8ceb93c86d2197ac9990568cf0e341bc063
kechin.vv <kechinv1@rambler.ru>
2023-09-18 17:05:48 +0300

    init StringBuffer_Tests

---
+++ src/main/java/tests/java/lang/StringBuffer_Tests.java
@@ -0,0 +1,358 @@
+package tests.java.lang;
+
+
+import java.lang.StringBuffer;
+
+public final class StringBuffer_Tests {
+
+    // internal variables
+
+    // constructors
+
+    // StringBufferAutomaton::StringBuffer
+    public static int test_StringBuffer_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::StringBuffer (CharSequence)
+    public static int test_StringBuffer_1(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::StringBuffer (String)
+    public static int test_StringBuffer_2(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::StringBuffer (int)
+    public static int test_StringBuffer_3(final int execution) {
+        return -1;
+    }
+
+
+    // static methods
+
+    // methods
+
+    // StringBufferAutomaton::append (CharSequence)
+    public static int test_append_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (CharSequence, int, int)
+    public static int test_append_1(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (Object)
+    public static int test_append_2(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (String)
+    public static int test_append_3(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (StringBuffer)
+    public static int test_append_4(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (boolean)
+    public static int test_append_5(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (char)
+    public static int test_append_6(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (char[])
+    public static int test_append_7(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (char[], int, int)
+    public static int test_append_8(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (double)
+    public static int test_append_9(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (float)
+    public static int test_append_10(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (int)
+    public static int test_append_11(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::append (long)
+    public static int test_append_12(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::appendCodePoint (int)
+    public static int test_appendCodePoint_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::capacity
+    public static int test_capacity_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::charAt (int)
+    public static int test_charAt_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::chars
+    public static int test_chars_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::codePointAt (int)
+    public static int test_codePointAt_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::codePointBefore (int)
+    public static int test_codePointBefore_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::codePointCount (int, int)
+    public static int test_codePointCount_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::codePoints
+    public static int test_codePoints_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::compareTo (StringBuffer)
+    public static int test_compareTo_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::delete (int, int)
+    public static int test_delete_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::deleteCharAt (int)
+    public static int test_deleteCharAt_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::ensureCapacity (int)
+    public static int test_ensureCapacity_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::getChars (int, int, char[], int)
+    public static int test_getChars_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::indexOf (String)
+    public static int test_indexOf_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::indexOf (String, int)
+    public static int test_indexOf_1(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, CharSequence)
+    public static int test_insert_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, CharSequence, int, int)
+    public static int test_insert_1(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, Object)
+    public static int test_insert_2(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, String)
+    public static int test_insert_3(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, boolean)
+    public static int test_insert_4(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, char)
+    public static int test_insert_5(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, char[])
+    public static int test_insert_6(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, char[], int, int)
+    public static int test_insert_7(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, double)
+    public static int test_insert_8(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, float)
+    public static int test_insert_9(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, int)
+    public static int test_insert_10(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::insert (int, long)
+    public static int test_insert_11(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::lastIndexOf (String)
+    public static int test_lastIndexOf_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::lastIndexOf (String, int)
+    public static int test_lastIndexOf_1(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::length
+    public static int test_length_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::offsetByCodePoints (int, int)
+    public static int test_offsetByCodePoints_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::replace (int, int, String)
+    public static int test_replace_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::reverse
+    public static int test_reverse_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::setCharAt (int, char)
+    public static int test_setCharAt_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::setLength (int)
+    public static int test_setLength_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::subSequence (int, int)
+    public static int test_subSequence_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::substring (int)
+    public static int test_substring_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::substring (int, int)
+    public static int test_substring_1(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::toString
+    public static int test_toString_0(final int execution) {
+        return -1;
+    }
+
+
+    // StringBufferAutomaton::trimToSize
+    public static int test_trimToSize_0(final int execution) {
+        return -1;
+    }
+
+}
+
