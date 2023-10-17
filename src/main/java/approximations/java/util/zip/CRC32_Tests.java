51260fac35e05e08aea9d2db401ee6690b2047fa
kechin.vv <kechinv1@rambler.ru>
2023-10-17 14:44:00 +0300

    init crc32 tests

---
+++ src/main/java/approximations/java/util/zip/CRC32_Tests.java
@@ -0,0 +1,57 @@
+package approximations.java.util.zip;
+
+import approximations.Test;
+
+public final class CRC32_Tests {
+
+    // internal variables
+
+    // constructors
+
+    // CRC32Automaton::CRC32
+    public static int test_CRC32_0(final int execution) {
+        return -1;
+    }
+
+
+    // static methods
+
+    // methods
+
+    // CRC32Automaton::getValue
+    public static int test_getValue_0(final int execution) {
+        return -1;
+    }
+
+
+    // CRC32Automaton::reset
+    @Test(executionMax = 2)
+    public static int test_reset_0(final int execution) {
+        return -1;
+    }
+
+
+    // CRC32Automaton::update (ByteBuffer)
+    public static int test_update_0(final int execution) {
+        return -1;
+    }
+
+
+    // CRC32Automaton::update (byte[])
+    public static int test_update_1(final int execution) {
+        return -1;
+    }
+
+
+    // CRC32Automaton::update (byte[], int, int)
+    public static int test_update_2(final int execution) {
+        return -1;
+    }
+
+
+    // CRC32Automaton::update (int)
+    public static int test_update_3(final int execution) {
+        return -1;
+    }
+
+}
